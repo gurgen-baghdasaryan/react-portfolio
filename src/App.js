f75f1460a7fb7add796117659010619f3a85a2f0
gugohh <oohhgug@gmail.com>
2021-12-11 18:56:47 +0100

    working on the left side ov the navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home"
+import Contact from "./components/Contact/Contact"
+import Portfolio from "./components/Portfolio/Portfolio"
+import Review from "./components/Review/Review"
+import Works from "./components/Works/Works"
+
+import "../src/app.scss"
+
+
 function App() {
+
   return (
     <div className="app">
-      ay ay ay
+      <NavBar />
+      <div className="sections">
+        <Home />
+        <Portfolio />
+        <Works />
+        <Review />
+        <Contact />
+
+      </div>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Person, Mail } from "@material-ui/icons";
+
 import './NavBar.scss'
 
 const NavBar = () => {
@@ -6,12 +8,19 @@ const NavBar = () => {
     <div className="navbar" >
       <div className="wrapper">
         <div className="left">
-           <a href="#home" className='logo'>gugscode</a>
-
+          <a href="#home" className='logo'>gugscode</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+34 685 93 93 86</span>
+          </div>
+          <div  className="itemContainer">
+            <Mail className="icon"/>
+            <span>gugdevelop@gmail.com</span>
+          </div>
         </div>
 
         <div className="right">
-              from right
+          from right
         </div>
       </div>
     </div>
