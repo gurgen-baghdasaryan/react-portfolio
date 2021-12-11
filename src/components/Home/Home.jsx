99953018a83dfd644118a2dc63bc995d667d0b87
gugohh <oohhgug@gmail.com>
2021-12-11 22:54:01 +0100

    added folder assets in public and already working on Home Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,18 @@ import './Home.scss'
 const Home = () => {
   return (
     <div className="home" id="home">
-      from home
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/mynft.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There! I'm</h2>
+          <h1>Gurgen</h1>
+          <h3>Full Stack <span>Web Developer</span></h3>
+        </div>
+      </div>
     </div>
   );
 };
